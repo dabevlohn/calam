daddde0249eddf551efb1d8eb9d0c6ad19f3060c
Aran Varun <vir@tue.su>
2024-09-03 22:59:00 +0300

    it works with tokio stream

--- examples/tokio_stream.rs
+++ examples/tokio_stream.rs
@@ -1,27 +1,44 @@
 use std::error::Error;
-use std::io;
+use std::fs::File;
+use std::io::prelude::*;
+use std::str;
 use tokio::net::TcpStream;
 
 //const PING: &[u8; 6] = b"zPING\0";
-const VERSION: &[u8; 9] = b"zVERSION\0";
+//const VERSION: &[u8; 9] = b"zVERSION\0";
+const INSTREAM: &[u8; 10] = b"zINSTREAM\0";
+const END_OF_STREAM: &[u8; 4] = &[0, 0, 0, 0];
+const DEFAULT_CHUNK_SIZE: usize = 4096;
+const FILE_PATH: &str = "examples/eicar.com";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    // Connect to a peer
+    let mut file = File::open(FILE_PATH)?;
+    let mut buffer = vec![0; DEFAULT_CHUNK_SIZE];
     let stream = TcpStream::connect("127.0.0.1:3310").await?;
 
     loop {
-        // Wait for the socket to be writable
         stream.writable().await?;
 
         // Try to write data, this may still fail with `WouldBlock`
         // if the readiness event is a false positive.
-        match stream.try_write(VERSION) {
+        match stream.try_write(INSTREAM) {
             Ok(n) => {
+                // TODO: implement logging
                 println!("write {} bytes", n);
+                loop {
+                    let len = file.read(&mut buffer[..])?;
+                    if len != 0 {
+                        stream.try_write(&(len as u32).to_be_bytes())?;
+                        stream.try_write(&buffer[..len])?;
+                    } else {
+                        stream.try_write(END_OF_STREAM)?;
+                        break;
+                    }
+                }
                 break;
             }
-            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
                 continue;
             }
             Err(e) => {
@@ -30,7 +47,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
         }
     }
 
-    //let mut response = Vec::new();
     let mut response = vec![0; 1024];
 
     loop {
@@ -45,7 +61,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 response.truncate(n);
                 break;
             }
-            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
                 continue;
             }
             Err(e) => {
@@ -54,7 +70,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         }
     }
 
-    println!("{:?}", response);
+    println!("{:?}", str::from_utf8(&response).unwrap());
 
     Ok(())
 }
