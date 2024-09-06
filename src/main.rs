a4ec2cf0f7d13176c9df1f5c8541a584649dfd33
Aran Varun <vir@tue.su>
2024-09-06 17:24:46 +0300

    ClamAV v1.0.6 compatible command protocol

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,93 @@
-mod actor;
-mod cli;
-mod clmd;
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::net::TcpListener;
+use tokio::sync::mpsc;
+
 mod message;
+use message::{BuyOrder, Message, OrderBookActor};
+mod tracker;
+use tracker::{TrackerActor, TrackerMessage, VersionTrackerActor};
 
-use eyre::Error;
-use std::process::ExitCode;
+#[tokio::main]
+async fn main() {
+    let addr = "127.0.0.1:3311".to_string();
 
-use crate::cli::Cli;
-use crate::clmd::clam_scan;
-use clap::Parser;
+    let socket = TcpListener::bind(&addr).await.unwrap();
+    println!("Listening on: {}", addr);
 
-fn run() -> eyre::Result<()> {
-    let args = Cli::parse();
+    let (tx, rx) = mpsc::channel::<Message>(1);
+    let (tracker_tx, tracker_rx) = mpsc::channel::<TrackerMessage>(1);
+    let tracker_tx_one = tracker_tx.clone();
 
-    match args.command {
-        cli::Commands::Scan(scan) => {
-            clam_scan(scan.address, scan.port, scan.file).expect("cannot scan file");
-        }
+    tokio::spawn(async {
+        TrackerActor::new(tracker_rx).run().await;
+    });
+    tokio::spawn(async move {
+        let order_book_actor = OrderBookActor::new(rx, tracker_tx_one.clone(), 20.0);
+        order_book_actor.run().await;
+    });
+    println!("Order book actor running now");
+
+    while let Ok((mut stream, peer)) = socket.accept().await {
+        println!("Incoming connection from: {}", peer.to_string());
+        let tx_one = tx.clone();
+        let tracker_tx_two = tracker_tx.clone();
+        tokio::spawn(async move {
+            println!("thread {} starting", peer.to_string());
+            let (reader, mut writer) = stream.split();
+            let mut buf_reader = BufReader::new(reader);
+            let mut buf = vec![];
+
+            loop {
+                match buf_reader.read_until(b'\0', &mut buf).await {
+                    Ok(n) => {
+                        if n == 0 {
+                            println!("EOF received");
+                            break;
+                        }
+
+                        let buf_string = String::from_utf8_lossy(&buf);
+                        let data: Vec<String> = buf_string
+                            .split("z")
+                            .map(|x| x.to_string().replace("\0", ""))
+                            .collect();
+                        println!("here is the data {:?}", data);
+                        let command = data[1].clone();
+
+                        match command.as_str() {
+                            "BUY" => {
+                                println!("buy order command processed");
+                                //let amount = data[1].parse::<f32>().unwrap();
+                                let amount = 32.0;
+                                let order_actor =
+                                    BuyOrder::new("ticker".to_string(), amount, tx_one.clone());
+                                //    BuyOrder::new(data[2].clone(), amount, tx_one.clone());
+                                println!("{}: {}", order_actor.ticker, order_actor.amount);
+                                order_actor.send().await;
+                            }
+                            "VERSION" => {
+                                println!("get version command processed");
+                                let get_actor = VersionTrackerActor {
+                                    sender: tracker_tx_two.clone(),
+                                };
+                                let state = get_actor.send().await;
+                                println!("sending back: {:?}", state);
+                                writer.write_all(state.as_bytes()).await.unwrap();
+                            }
+                            "PING" => {
+                                println!("PING command processed");
+                                writer.write_all(b"PONG\0").await.unwrap();
+                            }
+                            _ => {
+                                panic!("{} command not supported", command);
+                            }
+                        }
+                        buf.clear();
+                    }
+                    Err(e) => println!("Error receiving message: {}", e),
+                }
+            }
+
+            println!("thread {} finishing", peer.to_string());
+        });
     }
-
-    Ok(())
-}
-
-fn main() -> eyre::Result<ExitCode> {
-    color_eyre::install()?;
-
-    if let Err(err) = run() {
-        // User-facing errors should not show a stack trace.
-        if let Some(user_err) = err.downcast_ref::<Error>() {
-            eprintln!("{}", user_err);
-            return Ok(ExitCode::FAILURE);
-        }
-
-        return Err(err);
-    }
-
-    Ok(ExitCode::SUCCESS)
 }
--- src/message.rs
+++ src/message.rs
@@ -1,3 +1,14 @@
+use crate::tracker::{Command, TrackerMessage};
+use tokio::sync::{mpsc, oneshot};
+
+#[derive(Debug)]
+pub struct Message {
+    pub order: String,
+    pub ticker: String,
+    pub amount: f32,
+    pub respond_to: oneshot::Sender<u32>,
+}
+
 pub struct BuyOrder {
     pub order: String,
     pub ticker: String,
@@ -27,6 +38,60 @@ impl BuyOrder {
         match recv.await {
             Ok(outcome) => println!("here is the outcome: {}", outcome),
             Err(e) => println!("{}", e),
+        }
+    }
+}
+
+pub struct OrderBookActor {
+    pub receiver: mpsc::Receiver<Message>,
+    pub sender: mpsc::Sender<TrackerMessage>,
+    pub total_invested: f32,
+    pub investment_cap: f32,
+}
+
+impl OrderBookActor {
+    pub(crate) fn new(
+        receiver: mpsc::Receiver<Message>,
+        sender: mpsc::Sender<TrackerMessage>,
+        investment_cap: f32,
+    ) -> Self {
+        Self {
+            receiver,
+            sender,
+            total_invested: 0.0,
+            investment_cap,
+        }
+    }
+
+    async fn handle_message(&mut self, message: Message) {
+        if message.amount + self.total_invested >= self.investment_cap {
+            println!(
+                "rejecting purchase, total invested: {}",
+                self.total_invested
+            );
+            let _ = message.respond_to.send(0);
+        } else {
+            self.total_invested += message.amount;
+            println!(
+                "processing purchase, total invested: {}",
+                self.total_invested
+            );
+            let _ = message.respond_to.send(1);
+
+            let (send, _) = oneshot::channel();
+            let tracker_message = TrackerMessage {
+                command: Command::BUY(message.ticker, message.amount),
+                respond_to: send,
+            };
+
+            let _ = self.sender.send(tracker_message).await;
+        }
+    }
+
+    pub(crate) async fn run(mut self) {
+        println!("actor is running");
+        while let Some(msg) = self.receiver.recv().await {
+            self.handle_message(msg).await;
         }
     }
 }
