3426d47f4ea48d8aab03445493552f653bd86399
Aran Varun <vir@tue.su>
2024-09-08 00:01:26 +0300

    reads first bytes for command and last bytes for EOF

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use message::{FileOrder, Message, OrderBookActor};
 mod tracker;
 use tracker::{TrackerActor, TrackerMessage, VersionTrackerActor};
 
-//const END_OF_STREAM: &[u8; 4] = &[0, 0, 0, 0];
+const END_OF_STREAM: &[u8; 4] = &[0, 0, 0, 0];
 
 #[tokio::main]
 async fn main() {
@@ -34,12 +34,11 @@ async fn main() {
         let tx_one = tx.clone();
         let tracker_tx_two = tracker_tx.clone();
         tokio::spawn(async move {
-            println!("thread {} starting", peer.to_string());
+            //println!("thread {} starting", peer.to_string());
             let (reader, mut writer) = stream.split();
             let mut total_bytes_read = vec![];
-            let bytes_to_read_per_attempt = 16;
+            let bytes_to_read_per_attempt = 1024;
             let mut read_attempt_nr = 0;
-            let mut data: Vec<String> = vec![];
             let mut command = "zINSTREAM".to_string();
 
             loop {
@@ -50,69 +49,75 @@ async fn main() {
                 let nr = match reader.try_read(&mut cur_buffer) {
                     Ok(nr) => {
                         if nr == 0 {
-                            println!("0000 EOF received");
+                            println!("EOF received");
                             break;
                         }
 
                         if read_attempt_nr == 1 {
-                            let buf_string = String::from_utf8_lossy(&cur_buffer);
-                            data = buf_string.split("\0").map(|x| x.to_string()).collect();
-                            println!("here is the data {:?}", data[0]);
+                            let first10 = cur_buffer.as_slice()[0..9].to_vec();
+                            let buf_string = String::from_utf8_lossy(&first10);
+                            let data: Vec<String> = buf_string.split("\0").map(|x| x.to_string()).collect();
+                            //println!("first attempt data {:?}", data[0]);
                             command = data[0].clone();
+                        }
+
+                        let last4 = cur_buffer.as_slice()[cur_buffer.len()-4..].to_vec();
+                        if last4 == END_OF_STREAM {
+                            println!("0000 EOF received");
+                            // TODO: refactor with queues
+                            //
+                            match command.as_str() {
+                                "BUY" => {
+                                    println!("BUY command processed");
+                                    let order_actor = FileOrder::new("ticker".to_string(), 32.0, tx_one.clone());
+                                    println!("{}: {}", order_actor.ticker, order_actor.amount);
+                                    order_actor.send().await;
+                                }
+                                "zINSTREAM" => {
+                                    //println!("file order command processed");
+                                    // !!! Fictive response !!!
+                                    //
+                                    writer
+                                        .write_all(b"stream: Win.Test.EICAR_HDB-1 FOUND\0")
+                                        .await
+                                        .unwrap();
+                                }
+                                "zVERSION" => {
+                                    println!("get version command processed");
+                                    let get_actor = VersionTrackerActor {
+                                        sender: tracker_tx_two.clone(),
+                                    };
+                                    let state = get_actor.send().await;
+                                    println!("sending back: {:?}", state);
+                                    writer.write_all(state.as_bytes()).await.unwrap();
+                                }
+                                "zPING" => {
+                                    println!("PING command processed");
+                                    writer.write_all(b"PONG\0").await.unwrap();
+                                }
+                                _ => {
+                                    panic!("{} command not supported", command);
+                                }
+                            }
+                            writer.flush().await.unwrap();
+
+                            break;
                         }
                         nr
                     }
                     Err(ref e)
-                        //if e.kind() == ErrorKind::WouldBlock || e.kind() == ErrorKind::TimedOut =>
+                        if e.kind() == ErrorKind::WouldBlock || e.kind() == ErrorKind::TimedOut =>
                         //if e.kind() == ErrorKind::WouldBlock =>
-                        if e.kind() == ErrorKind::TimedOut =>
+                        //if e.kind() == ErrorKind::TimedOut =>
                     {
                         println!("--- Read attempt timed out");
-                        // TODO: refactor with queues
-                        //
-                        match command.as_str() {
-                            "BUY" => {
-                                println!("BUY command processed");
-                                let amount = data[1].parse::<f32>().unwrap();
-                                let order_actor =
-                                    FileOrder::new(data[2].clone(), amount, tx_one.clone());
-                                println!("{}: {}", order_actor.ticker, order_actor.amount);
-                                order_actor.send().await;
-                            }
-                            "zINSTREAM" => {
-                                println!("file order command processed");
-                                // !!! Fictive response !!!
-                                //
-                                writer
-                                    .write_all(b"stream: Win.Test.EICAR_HDB-1 FOUND\0")
-                                    .await
-                                    .unwrap();
-                            }
-                            "zVERSION" => {
-                                println!("get version command processed");
-                                let get_actor = VersionTrackerActor {
-                                    sender: tracker_tx_two.clone(),
-                                };
-                                let state = get_actor.send().await;
-                                println!("sending back: {:?}", state);
-                                writer.write_all(state.as_bytes()).await.unwrap();
-                            }
-                            "zPING" => {
-                                println!("PING command processed");
-                                writer.write_all(b"PONG\0").await.unwrap();
-                            }
-                            _ => {
-                                panic!("{} command not supported", command);
-                            }
-                        }
-                        return;
+                        break;
                     }
                     Err(e) => {
                         println!("Error receiving message: {}", e);
-                        return;
+                        break;
                     }
                 };
-                println!(">>> {}", nr);
                 cur_buffer.truncate(nr);
 
                 // TODO: save file and task in queue
@@ -120,7 +125,7 @@ async fn main() {
                 total_bytes_read.append(&mut cur_buffer);
             }
 
-            println!("thread {} finishing", peer.to_string());
+            //println!("thread {} finishing", peer.to_string());
         });
     }
 }
