6a7fe486263188caad26f916f2f0f6d72a8747cc
Aran Varun <vir@tue.su>
2024-09-14 02:13:55 +0300

    works without spawn

--- src/filereceiver.rs
+++ src/filereceiver.rs
@@ -1,37 +1,119 @@
-use std::path::Path;
-use tokio::{
-    fs::File,
-    io::{self, AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt},
-    net::{TcpStream, ToSocketAddrs},
-};
+use std::path::PathBuf;
+use tokio::io::{AsyncWriteExt, ErrorKind};
+use tokio::net::TcpListener;
 
-use tokio_stream::{Stream, StreamExt};
+use super::END_OF_STREAM;
 
-use super::{IoResult, DEFAULT_CHUNK_SIZE};
+pub struct FileReceiver {
+    pub socket: TcpListener,
+    pub filepath: PathBuf,
+}
 
-async fn save_stream<
-    S: Stream<Item = Result<bytes::Bytes, std::io::Error>>,
-    RW: AsyncRead + AsyncWrite + Unpin,
->(
-    input_stream: S,
-    chunk_size: Option<usize>,
-    mut output_stream: RW,
-) -> IoResult {
-    let chunk_size = chunk_size
-        .unwrap_or(DEFAULT_CHUNK_SIZE)
-        .min(u32::MAX as usize);
-
-    let mut input_stream = std::pin::pin!(input_stream);
-
-    while let Some(bytes) = input_stream.next().await {
-        let bytes = bytes?;
-        let bytes = bytes.as_ref();
-        for chunk in bytes.chunks(chunk_size) {
-            let len = chunk.len();
-            //output_stream.write_all(&(len as u32).to_be_bytes()).await?;
-            //output_stream.write_all(chunk).await?;
+impl FileReceiver {
+    pub fn new(socket: TcpListener) -> Self {
+        // TODO: filepath from env
+        //
+        Self {
+            socket,
+            filepath: PathBuf::from("/tmp"),
         }
     }
 
-    Ok(())
+    fn handle_filestream(&mut self) {
+        println!("got file!");
+    }
+
+    pub async fn run(self) {
+        println!("FileReceiver is running");
+        while let Ok((mut stream, peer)) = self.socket.accept().await {
+            println!("Incoming connection from: {}", peer.to_string());
+            tokio::spawn(async move {
+                println!("thread {} starting", peer.to_string());
+                let (reader, mut writer) = stream.split();
+                let mut total_bytes_read = vec![];
+                let bytes_to_read_per_attempt = 1024;
+                let mut read_attempt_nr = 0;
+                let mut command = "zINSTREAM".to_string();
+
+                loop {
+                    read_attempt_nr += 1;
+                    // println!("Read attempt nr {read_attempt_nr}");
+                    let mut cur_buffer = vec![0; bytes_to_read_per_attempt];
+
+                    let nr = match reader.try_read(&mut cur_buffer) {
+                        Ok(nr) => {
+                            if nr == 0 {
+                                println!("EOF received");
+                                break;
+                            }
+
+                            if read_attempt_nr == 1 {
+                                let first10 = cur_buffer.as_slice()[0..9].to_vec();
+                                let buf_string = String::from_utf8_lossy(&first10);
+                                let data: Vec<String> =
+                                    buf_string.split("\0").map(|x| x.to_string()).collect();
+                                //println!("first attempt data {:?}", data[0]);
+                                command = data[0].clone();
+                            }
+
+                            let last4 = cur_buffer.as_slice()[cur_buffer.len() - 4..].to_vec();
+                            if last4 == END_OF_STREAM {
+                                println!("0000 EOF received");
+                                // TODO: refactor with queues
+                                //
+                                match command.as_str() {
+                                    "zINSTREAM" => {
+                                        println!("file order command processed");
+                                        // !!! Fictive response !!!
+                                        //
+                                        //self.handle_filestream();
+                                        writer
+                                            .write_all(b"stream: Win.Test.EICAR_HDB-1 FOUND\0")
+                                            .await
+                                            .unwrap();
+                                    }
+                                    "zVERSION" => {
+                                        //println!("get version command processed");
+                                        writer
+                                            .write_all(b"ClamAV 1.0.6 compatible\0")
+                                            .await
+                                            .unwrap();
+                                    }
+                                    "zPING" => {
+                                        println!("PING command processed");
+                                        writer.write_all(b"PONG\0").await.unwrap();
+                                    }
+                                    _ => {
+                                        panic!("{} command not supported", command);
+                                    }
+                                }
+                                writer.flush().await.unwrap();
+
+                                break;
+                            }
+                            nr
+                        }
+                        Err(ref e)
+                            if e.kind() == ErrorKind::WouldBlock
+                                || e.kind() == ErrorKind::TimedOut =>
+                        {
+                            //println!("Read attempt timed out");
+                            break;
+                        }
+                        Err(e) => {
+                            println!("Error receiving message: {}", e);
+                            break;
+                        }
+                    };
+                    cur_buffer.truncate(nr);
+
+                    // TODO: save file and task in queue
+                    //
+                    total_bytes_read.append(&mut cur_buffer);
+                }
+
+                println!("thread {} finishing", peer.to_string());
+            });
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,103 +1,22 @@
-use std::thread;
-use tokio::io::{AsyncWriteExt, ErrorKind};
+use std::time::Duration;
 use tokio::net::TcpListener;
+use tokio::{task, time};
+
+mod filereceiver;
+use filereceiver::FileReceiver;
 
 const END_OF_STREAM: &[u8; 4] = &[0, 0, 0, 0];
 
 #[tokio::main]
 async fn main() {
+    pretty_env_logger::init();
     let addr = "127.0.0.1:3310".to_string();
     let socket = TcpListener::bind(&addr).await.unwrap();
-    thread::spawn(move || filereceiver(socket));
-}
-
-async fn filereceiver(socket: TcpListener) {
-    while let Ok((mut stream, peer)) = socket.accept().await {
-        println!("Incoming connection from: {}", peer.to_string());
-        tokio::spawn(async move {
-            //println!("thread {} starting", peer.to_string());
-            let (reader, mut writer) = stream.split();
-            let mut total_bytes_read = vec![];
-            let bytes_to_read_per_attempt = 1024;
-            let mut read_attempt_nr = 0;
-            let mut command = "zINSTREAM".to_string();
-
-            loop {
-                read_attempt_nr += 1;
-                // println!("Read attempt nr {read_attempt_nr}");
-                let mut cur_buffer = vec![0; bytes_to_read_per_attempt];
-
-                let nr = match reader.try_read(&mut cur_buffer) {
-                    Ok(nr) => {
-                        if nr == 0 {
-                            println!("EOF received");
-                            break;
-                        }
-
-                        if read_attempt_nr == 1 {
-                            let first10 = cur_buffer.as_slice()[0..9].to_vec();
-                            let buf_string = String::from_utf8_lossy(&first10);
-                            let data: Vec<String> =
-                                buf_string.split("\0").map(|x| x.to_string()).collect();
-                            //println!("first attempt data {:?}", data[0]);
-                            command = data[0].clone();
-                        }
-
-                        let last4 = cur_buffer.as_slice()[cur_buffer.len() - 4..].to_vec();
-                        if last4 == END_OF_STREAM {
-                            println!("0000 EOF received");
-                            // TODO: refactor with queues
-                            //
-                            match command.as_str() {
-                                "zINSTREAM" => {
-                                    //println!("file order command processed");
-                                    // !!! Fictive response !!!
-                                    //
-                                    writer
-                                        .write_all(b"stream: Win.Test.EICAR_HDB-1 FOUND\0")
-                                        .await
-                                        .unwrap();
-                                }
-                                "zVERSION" => {
-                                    //println!("get version command processed");
-                                    writer
-                                        .write_all(b"ClamAV 1.0.6 compatible\0")
-                                        .await
-                                        .unwrap();
-                                }
-                                "zPING" => {
-                                    println!("PING command processed");
-                                    writer.write_all(b"PONG\0").await.unwrap();
-                                }
-                                _ => {
-                                    panic!("{} command not supported", command);
-                                }
-                            }
-                            writer.flush().await.unwrap();
-
-                            break;
-                        }
-                        nr
-                    }
-                    Err(ref e)
-                        if e.kind() == ErrorKind::WouldBlock || e.kind() == ErrorKind::TimedOut =>
-                    {
-                        //println!("Read attempt timed out");
-                        break;
-                    }
-                    Err(e) => {
-                        println!("Error receiving message: {}", e);
-                        break;
-                    }
-                };
-                cur_buffer.truncate(nr);
-
-                // TODO: save file and task in queue
-                //
-                total_bytes_read.append(&mut cur_buffer);
-            }
-
-            //println!("thread {} finishing", peer.to_string());
-        });
-    }
+    //task::spawn(async move {
+    FileReceiver::new(socket).run().await;
+    //time::sleep(Duration::from_secs(3)).await;
+    //});
+    //loop {
+    //    time::sleep(Duration::from_secs(120)).await;
+    //}
 }
